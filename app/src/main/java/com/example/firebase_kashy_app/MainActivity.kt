20733a986ac3dedc4c2fc98ed9c2b10c3aab6e70
Kashyroyalty <sharonnagi13@gmail.com>
2023-04-28 13:21:19 +0300

    car dealer diusplay

--- app/src/main/java/com/example/firebase_kashy_app/MainActivity.kt
+++ app/src/main/java/com/example/firebase_kashy_app/MainActivity.kt
@@ -1,34 +1,87 @@
 package com.example.firebase_kashy_app
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var carmake:EditText
+    lateinit var carmodel:EditText
+    lateinit var carprice:EditText
+    lateinit var upload_photo:Button
+    lateinit var upload_data:Button
+    lateinit var view_uploads:Button
 
-    lateinit var edt_user_inp:EditText
-    lateinit var btn_save_data:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edt_user_inp = findViewById(R.id.edtdata)
-        btn_save_data = findViewById(R.id.btnsave)
+        carmake = findViewById(R.id.edtmake)
+        carmodel = findViewById(R.id.edtmodel)
+        carprice= findViewById(R.id.edtprice)
+        upload_photo = findViewById(R.id.btncarphoto)
+        upload_data = findViewById(R.id.btncardata)
+        view_uploads = findViewById(R.id.btnviewcars)
 
-        //initialize
         var database = FirebaseDatabase.getInstance()
-        var  databaseRef = database.reference
+        var databaseref =  database.getReference("cars")
 
-        btn_save_data.setOnClickListener {
 
-            var user_data = edt_user_inp.text.toString().trim()
-            //Toast.makeText(this, user_data, Toast.LENGTH_SHORT).show()
-            databaseRef.setValue(user_data)
+
+        upload_photo.setOnClickListener {
+
+
+
+        }
+
+
+
+        upload_data.setOnClickListener {
+            var carmake = carmake.text.toString().trim()
+            var carmodel = carmodel.text.toString().trim()
+            var carprice = carprice.text.toString().trim()
+
+            if(carmake.isEmpty()|| carmodel.isEmpty()|| carprice.isEmpty()){
+                Toast.makeText(this, "Cannot submit an empty form", Toast.LENGTH_SHORT).show()
+            }else{
+                //saving ino to firebase db
+                var usercar = Car(carmake,carmodel,carprice)
+                var ref = FirebaseDatabase.getInstance().getReference().child("Cars")
+
+                ref.setValue(usercar).addOnCompleteListener{
+
+                    if (it.isSuccessful){
+                        Toast.makeText(this,"Car Data Uploaded Successfully",
+                            Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(this,"Failed tO Save Car Info",
+                            Toast.LENGTH_LONG).show()
+                    }
+
+                }
+
+
+
+            }
+
+
+
+        }
+        view_uploads.setOnClickListener {
+
+
+
+        }
+
+
+
+
 
         }
     }
-}
